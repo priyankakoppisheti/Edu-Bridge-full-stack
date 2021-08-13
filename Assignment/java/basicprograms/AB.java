1f33484b6013120c39651f8dc1e35cf4cfdd7487
priyankakoppisheti <87064014+priyankakoppisheti@users.noreply.github.com>
2021-08-13 16:53:50 +0530

    Add files via upload

---
+++ Assignment/java/basicprograms/AB.java
@@ -0,0 +1,21 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class AB {
+
+	public static void main(String[] args) {
+		
+ArrayList<String> a=new ArrayList<String>();
+a.add("priya");
+a.add("mouny");
+a.add("harika");
+a.add("shine");
+System.out.println(a);
+for (String s : a)
+{
+	System.out.println(s+"");
+}
+	}
+
+}
