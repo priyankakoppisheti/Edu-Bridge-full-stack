9f779f8b52193a499916d55489e81138da16f338
priyankakoppisheti <87064014+priyankakoppisheti@users.noreply.github.com>
2021-08-13 16:54:57 +0530

    Add files via upload

---
+++ Assignment/java/basicprograms/factory.java
@@ -0,0 +1,12 @@
+package collection;
+import java.util.List; 
+public class factory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list = List.of("harika","priya","learner","khusbhu","gauri");  
+		for(String l:list) {  
+            System.out.println(l); 
+	}
+	}
+}
